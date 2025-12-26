d9b6563f082132cbe02d9f4864ee4e0df53c6ec2
AdmireLinK <64939678+AdmireLinK@users.noreply.github.com>
2025-12-27 03:40:05 +0800

    fix：JSON parse error乱码

--- character_tags_crawler/bangumi/dump_converter/redirect_checker.py
+++ character_tags_crawler/bangumi/dump_converter/redirect_checker.py
@@ -7,7 +7,6 @@ chdir_project_root()
 
 headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'Accept-Encoding': 'gzip, deflate, br',
     'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
     'User-Agent': 'Zzzyt/MoeRanker (https://github.com/Zzzzzzyt/MoeRanker)',
 }
